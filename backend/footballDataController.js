249f239bea07d6aa0be4e42accd75a05ddc9cbd5
Noud63 <noudvandun@gmail.com>
2023-02-21 17:32:54 +0100

    test

--- backend/footballDataController.js
+++ backend/footballDataController.js
@@ -11,13 +11,13 @@ const getFootballData = asyncHandler(async (req, res) => {
         await axios.get(URL, {
             headers: {
                 "X-Auth-Token": process.env.REACT_APP_API_KEY , 
-                "Access-Control-Allow-Origin": "*"
+                "Access-Control-Allow-Origin": "http://localhost:3000"
             }
         }),
         await axios.get(URL2, {
             headers: {
                 "X-Auth-Token": process.env.REACT_APP_API_KEY,
-                "Access-Control-Allow-Origin": "*"
+                "Access-Control-Allow-Origin": "http://localhost:3000"
             }
         })
     ])
