5cf42914531b936c00d9c4bd7f12f7ba9cf9e85b
Noud63 <noudvandun@gmail.com>
2024-05-28 12:55:24 +0200

    deployment

--- backend/footballDataRoute.js
+++ backend/footballDataRoute.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router = express.Router()
 const getFootballData = require('./footballDataController')
 
-router.route('/api').get(getFootballData)
+router.route('/').get(getFootballData)
 module.exports = router
