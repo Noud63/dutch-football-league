f428270dcef98f9bed2e278b31c86531b1eb4980
Noud63 <noudvandun@gmail.com>
2024-11-18 11:32:53 +0100

    new commit

--- backend/server.js
+++ backend/server.js
@@ -11,16 +11,16 @@ app.use(cors())
 app.use(express.json())
 dotenv.config()
 
-app.use(
-  cors({
-    origin: [
-      "https://dutch-football-league-8k8w.vercel.app/api",
-      "localhost:3000",
-    ],
-    methods: ["GET"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://dutch-football-league-8k8w.vercel.app/api",
+//       "localhost:3000",
+//     ],
+//     methods: ["GET"],
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.json("Hello");
@@ -30,11 +30,11 @@ app.use('/api/footballData', footballDataRoute)
 
 // Place after routes
 // Render => Serving the frontend
-app.use('/', express.static(path.join(__dirname, '../frontend/', 'build')))
+// app.use('/', express.static(path.join(__dirname, '../frontend/', 'build')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+// });
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
 
