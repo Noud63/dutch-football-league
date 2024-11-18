4117c4023387859e4f7f9e4abab4bd5e851ec5f9
Noud63 <noudvandun@gmail.com>
2024-11-18 12:08:00 +0100

    new commit

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/footballData', footballDataRoute)
 
 // Place after routes
 // Render => Serving the frontend
-app.use('/', express.static(path.join(__dirname, '../frontend/', 'build')))
+app.use('/', express.static(path.join(__dirname, '../frontend/build')))
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
