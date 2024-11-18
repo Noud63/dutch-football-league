0b3b5ecd14e4289798bd0de5ae8ca32f926f5347
Noud63 <noudvandun@gmail.com>
2024-11-18 14:19:37 +0100

    middleware

--- frontend/src/Context.js
+++ frontend/src/Context.js
@@ -27,7 +27,7 @@ const AllData = ({ children }) => {
 
         try {
             const response = await axios(
-              "https://dutch-football-league-api.vercel.app/api/footballData"
+            "/api/footballData"
             );
 
             console.log(response)
