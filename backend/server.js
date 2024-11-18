563d0e822b31d4c41aaa14fefff4d6ead8e6aace
Noud63 <noudvandun@gmail.com>
2024-11-18 14:17:42 +0100

    middleware

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,19 @@ const router = express.Router()
 const footballDataRoute = require('./footballDataRoute')
 const PORT = process.env.REACT_APP_PORT || 5000
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 dotenv.config()
 
-// app.use(
-//   cors({
-//     origin: [
-//       "https://dutch-football-league-df6m.vercel.app"
-//     ],
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [
+      "https://dutch-football-league-df6m.vercel.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json("Hello");
